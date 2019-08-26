e41b978d6d5d64e8799861a9358e4e2e5d54a204
Aimal <aimal.islam@gmail.com>
2019-08-26 17:18:31 +0430

    First Version of Website

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+// Email Submit
+// Note: filter_var() requires PHP 5.2.0 or Higher
+if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['subject']) && isset($_POST['message']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
+ 
+  // detect & prevent header injections
+  $test = "/(content-type|bcc:|cc:|to:)/i";
+  foreach ( $_POST as $key => $val ) {
+    if ( preg_match( $test, $val ) ) {
+      exit;
+    }
+  }
+  
+  //   Replace with your email
+  mail( "aimal.islam@gmail.com", $_POST['subject'], $_POST['message'], "From:" . $_POST['email'] );  
+}
+?>
